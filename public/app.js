aaa258d33d0f9dceb364e348909065258845dc57
Dennis Ruto <rutodeno@gmail.com>
2018-06-13 22:31:40 -0400

    rendered data to front-end

---
+++ public/app.js
@@ -0,0 +1,8 @@
+
+$.getJSON("/allarticles", function(data) {
+    for(var i = 0; i < data.length; i++) {
+        $("#articles").append("<p data-id'" +data[i]._id + "'>" +data[i].title + "<br/>" + data[i].author + "<br/>" + data[i].link + "</p>"); 
+    }
+
+    console.log(data);
+});
