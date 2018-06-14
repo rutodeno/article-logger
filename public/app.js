89e62eaf59a40f7ca487823d8dbe78ad3e91dfa9
Dennis Ruto <rutodeno@gmail.com>
2018-06-13 22:42:38 -0400

    creating an ajax call to render note section

--- public/app.js
+++ public/app.js
@@ -3,6 +3,21 @@ $.getJSON("/allarticles", function(data) {
     for(var i = 0; i < data.length; i++) {
         $("#articles").append("<p data-id'" +data[i]._id + "'>" +data[i].title + "<br/>" + data[i].author + "<br/>" + data[i].link + "</p>"); 
     }
-
     console.log(data);
 });
+
+$(document).on("click", "p", function() {
+    $("#notes").empty();
+
+    let thisId = $(this).attr("data-id");
+
+    $.ajax({
+        method: "GET",
+        url:"/allarticles"
+    })
+    .then(function(data) {
+        console.log(data);
+        $("#notes").append("<h2" + data[0].title + "</h2>");
+        $("#notes").append("")
+    })
+})
