d49d2b66d43a310656724e67508c0a2bee6ec65f
Dennis Ruto <rutodeno@gmail.com>
2018-06-24 22:45:32 -0400

    added styling to center image and navbar

--- public/app.js
+++ public/app.js
@@ -37,43 +37,16 @@ $(document).ready(function() {
 
             $(".modalHeader").html(data[0].title);
             $("#savenote").append("<data-id '" + data[0]._id + "'>");
-        })
 
-    })
-
-/*
-    $(document).on("click", "p", function() {
-       // $("#notes").empty();
-
-       $('.modal').modal('open');
-
-        let thisId = $(this).attr("data-id");
-
-        console.log(thisId)
-
-        $.ajax({
-            method: "GET",
-            url: "/allarticles/" + thisId
-        })
-        .then(function(data) {
-
-            
-
-
-            console.log(data);
-            
-            $("#notes").append("<h6 class='header'>" + data[0].title + "</h6>");
-            $("#notes").append("<input id='titleinput' name='title' >");
-            $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-            $("#notes").append("<button  class='btn waves-effect waves-light' data-id'" + data[0]._id + "' id='savenote'> Save Note</button> ");
-            
             if(data.note) {
                 $("#titleinput").val(data.note.title);
                 $("#bodyinput").val(data.note.body);
             }
-        });
-    }); 
-*/
+
+        })
+
+    })
+
     $(document).on("click", "#savenote", function() {
         let thisId = $(this).attr("data-id");
 
