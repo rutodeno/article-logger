418a81a7363a9ba75d633a1070a9df0d22056a65
Dennis Ruto <34184017+rutodeno@users.noreply.github.com>
2018-09-03 20:32:50 -0400

    added note.js to controller

---
+++ controllers/note.js
@@ -0,0 +1,24 @@
+let db = require('../models');
+
+module.exports = {
+    find: function(req, res) {
+        db.Note.find({_headlineId: req.params.id }).then(function(dbNote) {
+            res.json(dbNote);
+        });
+    },
+
+    // create
+
+    create: function(req, res) {
+        db.Note.create(req.body).then(function(dbNote) {
+            res.json(dbNote);
+        });
+    },
+
+    // delete
+    delete: function(req, res) {
+        db.Note.remove({_id: req.params.id }).then(function(dbNote) {
+            res.json(dbNote);
+        });
+    }
+};
