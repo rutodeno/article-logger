74632fb382a55f6b481f78c160072de00bbd5e71
Dennis Ruto <34184017+rutodeno@users.noreply.github.com>
2018-09-06 08:14:35 -0400

    added an empty alert message

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -7,20 +7,44 @@ $(document).ready(function() {
     initPage();
 
     function initPage() {
-
-
+        $.get("/api/articles?saved=false").then(function(data) {
+            console.log(data);
+            if (data && data.length) {
+                renderArticles(data);
+            } else {
+                renderEmpty();
+            }
+        })
     }
 
 
-
-    $.getJSON("/api/articles", function(data) {
+    function renderArticles(data) {
         for(var i = 0; i < data.length; i++) {
             var biggerDiv = $("<div data-id='" +data[i]._id+ "' class='card-panel'>");
             $(biggerDiv).append("<a href = https://www.desiringgod.org/" +data[i].link+" target='_blank'> <h4>" +data[i].title + "</h4> by " + data[i].author + "<br />"+ "</a> " +"<br />"); 
             $(biggerDiv).append("<button data-target='modal1' data-id='" +data[i]._id+ "' class='btn modal-trigger' id = 'modalTrigger'>Comment</button>");
-            $("#articles").append(biggerDiv);
+            articleContainer.append(biggerDiv);
         }
-    });
+    }
 
-
+    function renderEmpty() {
+        let emptyAlert = $(
+            [ 
+                "<div class='alert alert-warning text center'>",
+                "<h4>No new articles today, Sorry!!</h4>",
+                "</div>",
+                "<div class='card",
+                "<div class='card-header text center>",
+                "<h3What would you like to do?</h3>",
+                "</div>",
+                "<div class='card-body text-center'>",
+                "<h4><a class='scrape-new'>Try Scraping New Articles</a></h4>",
+                "<h4><a href='/saved'>Go to Saved Articles</a></h4>",
+                "</div>",
+                "</div>"
+            ].join("")
+        );
+        articleContainer.append(emptyAlert);
+    }
+    
 });
