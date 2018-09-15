7ca150353c003b8d52f2fd534e5773f62aa994ad
Dennis Ruto <34184017+rutodeno@users.noreply.github.com>
2018-09-15 17:56:11 -0400

    added several functions to saved.js

--- public/assets/javascript/saved.js
+++ public/assets/javascript/saved.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     let articleContainer = $(".article-container");
 
     $(document).on("click", ".btn.delete", handleArticleDelete);
@@ -10,8 +10,8 @@ $(document).ready(function() {
 
     function initPage() {
         articleContainer.empty();
-        $.get("/api/article?saved=true").then(function(data) {
-            if(data && data.length) {
+        $.get("/api/article?saved=true").then(function (data) {
+            if (data && data.length) {
                 renderArticles(data)
             } else {
                 renderEmpty();
@@ -27,6 +27,69 @@ $(document).ready(function() {
         }
 
         articleContainer.append(articleCards);
+    }
+
+    function createCard(article) {
+        let card = $(
+            [
+                "<div class='card'>",
+                "<div class='card-header'>",
+                "<h3>",
+                "<a class='article-link' target='_blank' href='" +article.url+ "'>", article.headline, "</a>",
+                "<a class='btn btn-danger delete'", "Delete From Saved", "</a>",
+                "<a class='btn btn-info notes'>Article Notes</a>",
+                "</h3>",
+                "</div>",
+                "</div>"
+            ].join("")
+        );
+
+        card.data("_id", article._id);
+        return card;
+    }
+
+    function renderEmpty() {
+        let emptyAlert =$(
+            [
+                "<div class='alert alert-warning text-center'>",
+                "<h4>We need new saved articles</h4>",
+                "</div>",
+                "<div class='card'>",
+                "<div class='card-header text-center'>",
+                "<h3>Please browse available articles</h3>",
+                "</div>",
+                "<div class='card-body text-center'>",
+                "<h4><a href='/'>Browse Articles</a></h4>",
+                "</div>",
+                "</div>"
+            ].join("")
+        );  
+        articleContainer.append(emptyAlert);
+    }
+
+    function handleArticleNotes() {
+
+    }
+
+    function handleNoteSave() {
+        let noteData;
+        let newNote = $(".bootbox-body textarea").val().trim();
+        if (newNote) {
+            noteData = { _headlineId: $(this).data("article")._id, noteText: newNote }
+            $.post("/api/notes", noteData).then(function () {
+                bootbox.hideAll();
+            });
+        }
+    }
+
+    function handleNoteDelete() {
+        let noteToDelete = $(this).data("_id");
+        $.ajax({
+            url: "/api/notes/" + noteToDelete,
+            method: "DELETE"
+        }).then(function () {
+            bootbox.hideAll();
+        })
     }
 
 });
